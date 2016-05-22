a9303af6d9030db635e800d3b003f2f59b3a7254
Mitchell <mgerb42@gmail.com>
2016-05-22 17:19:13 -0500

    new post - also fixed loading order of posts on index page

--- public/js/IndexController.js
+++ public/js/IndexController.js
@@ -1,6 +1,6 @@
 app.controller('IndexController', function($scope, $http) {
 
-    var title = ["1-4-16", "1-1-16", "12-29-15", "12-18-15", "10-28-15", "8-13-15", "7-28-15", "7-21-15"];
+    var title = ["5-22-16", "1-4-16", "1-1-16", "12-29-15", "12-18-15", "10-28-15", "8-13-15", "7-28-15", "7-21-15"];
     $scope.posts = [];
 
     for (p in title) {
@@ -9,10 +9,11 @@ app.controller('IndexController', function($scope, $http) {
 
         //use anonymous function calls to pass
         //postName to the http callback function
+        //pass value p to make sure $scope.posts is in order due to asynch function
         $http({
             method: 'GET',
             url: '/public/posts/' + postName + '.html'
-        }).then((function(postName) {
+        }).then((function(postName, p) {
             return function(response) {
 
                 var html = response.data;
@@ -23,9 +24,9 @@ app.controller('IndexController', function($scope, $http) {
                 partial.date = $(html).find('#date').html();
                 partial.intro = $(html).find('#intro').html();
 
-                $scope.posts.push(partial);
+                $scope.posts[p] = partial;
             }
-        })(postName), function errorCallback(response) {
+        })(postName, p), function errorCallback(response) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
             console.log("Error Loading Post");
