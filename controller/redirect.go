db4dbed39bf0ca4608a2bfbe6866583e1d3ae693
Mitchell Gerber <mgerb42@gmail.com>
2016-07-27 22:40:39 -0500

    Added new route

--- controller/redirect.go
+++ controller/redirect.go
@@ -7,14 +7,15 @@ import (
 
 // Redirect to discord
 func DiscordRedirect(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
 	http.Redirect(w, r, "https://discordapp.com/invite/0Z2tzxKECEj2BHwj", 301)
-
 }
 
 // Redirect to discord
 func VPNRedirect(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
 	http.Redirect(w, r, "https://mitchel.io:943", 301)
+}
 
+// Redirect to security cameras
+func CameraRedirect(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.Redirect(w, r, "https://24.118.44.161:8080/html/", 301)
 }
