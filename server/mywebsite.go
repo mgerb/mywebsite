d2cc34d3a063bffecc199907238874d636e49f90
mgerb42 <mgerb42@gmail.com>
2016-12-08 03:08:33 +0100

    updated header

--- server/mywebsite.go
+++ server/mywebsite.go
@@ -1,34 +1,38 @@
 package main
 
 import (
+	"github.com/NYTimes/gziphandler"
 	"log"
 	"net/http"
 	"strconv"
-	"github.com/NYTimes/gziphandler"
-	
+
 	"mywebsite/server/controller/api"
 	"mywebsite/server/db"
 	"mywebsite/server/route"
 	"mywebsite/server/utils"
 )
 
-func main(){
+func main() {
 	configurations := utils.ReadConfig()
 
 	db.Configure(configurations.Database)
 	api.Configure(configurations.Api)
 
 	db.Mongo.Connect()
-	
+
 	//register middleware
 	handle := gziphandler.GzipHandler(route.Routes())
-	
+
 	log.Println("Starting Server...")
+	log.Println(http.ListenAndServe(":"+strconv.Itoa(configurations.Port), handle))
+
+	/*  enable for TLS support
 	go func(){
 		log.Println(http.ListenAndServe(":"+strconv.Itoa(configurations.Port), handle))
 	}()
-	
+
 	if configurations.TLSCertFile != "" && configurations.TLSKeyFile != "" {
 		log.Println(http.ListenAndServeTLS(":"+strconv.Itoa(configurations.TLSPort), configurations.TLSCertFile, configurations.TLSKeyFile, handle))
 	}
+	*/
 }
